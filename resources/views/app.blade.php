9d600c8b84a4e625ef5a192726ced851a3b0123c
PC <elmeftouhi@extensionmedia.ma>
2022-06-21 23:19:42 +0100

    styling header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,8 +22,36 @@
             @include('layout.vertical_nav')
         </div>
         <div class="flex-1 pt-4 pr-4">
-            <div class="bg-white h-full" style="border-top-left-radius: 30px; border-top-right-radius: 30px">
-                content here
+            <div class="bg-white h-full flex flex-col" style="border-top-left-radius: 30px; border-top-right-radius: 30px">
+                <div class="bg-gray-150 w-full h-12 shadow" style="border-top-left-radius: 30px; border-top-right-radius: 30px">
+                    <div class="h-full border-b flex justify-between items-center px-4">
+                        <div class="">
+                            <i class="fa-solid fa-house"></i> 
+                            <span class="font-light">
+                                Dashboard
+                            </span>
+                        </div>
+                        <div class="flex gap-4">
+                            <div class="rounded-full h-8 w-8 pt-1 text-center cursor-pointer border hover:border-pink-100 hover:bg-gray-700 hover:text-white">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                            <div class="rounded-full h-8 w-8 pt-1 text-center cursor-pointer border hover:border-pink-100 hover:bg-gray-700 hover:text-white">
+                                <i class="fa-solid fa-bell"></i>
+                            </div>
+                            <div class="flex gap-2 items-center rounded-full h-8  px-2 text-center cursor-pointer text-white border border-gray-700 bg-gray-800 hover:border-pink-700 hover:bg-pink-100 hover:text-gray-700">
+                                <i class="fa-solid fa-user"></i>
+                                <span class="text-xs font-light">
+                                    Yassine
+                                </span>
+                            </div>
+                        </div>
+                    </div>              
+                </div>
+
+                <div class="flex-1 px-4 bg-gray-50">
+                  content  
+                </div>
+                
             </div>
         </div>
         
